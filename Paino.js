7722eca6ccef5645a40997eed565e4b4cee010e9
QADIR777 <128066326+QADIR777@users.noreply.github.com>
2024-04-26 13:13:44 +0500

    Painoo

---
+++ Paino.js
@@ -0,0 +1,59 @@
+function C4() {
+    document.getElementById("c4").play()   
+}
+function D4() {
+    document.getElementById("d4").play()   
+    
+}
+function E4() {
+    document.getElementById("e4").play()   
+    
+}
+function F4() {
+    document.getElementById("f4").play()   
+    
+}
+function G4() {
+    document.getElementById("g4").play()   
+    
+}
+function A4() {
+    document.getElementById("a4").play()   
+    
+}
+function B4() {
+    document.getElementById("b4").play()   
+    
+}
+function C5() {
+    document.getElementById("c5").play()   
+    
+}
+function D5() {
+    document.getElementById("d5").play()   
+    
+}
+function E5() {
+    document.getElementById("e5").play()   
+    
+}
+function F5() {
+    document.getElementById("f5").play()   
+    
+}
+function G5() {
+    document.getElementById("g5").play()   
+    
+}
+function A5() {
+    document.getElementById("a5").play()   
+    
+}
+function B5() {
+    document.getElementById("b5").play()   
+    
+}
+function C6() {
+    document.getElementById("c6").play()   
+    
+}
